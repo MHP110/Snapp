9e33c34992b07d7f5da83db4d716217f349d7142
Mohammad Hasan Pouhasani <76045848+MHP110@users.noreply.github.com>
2021-10-25 15:20:11 +0330

    Add Snap Project

---
+++ index.js
@@ -0,0 +1,21 @@
+const changeImgAsghar = document.getElementById(asghar-image).onchange = () => {if (screenX < '768px') {
+    document.
+    }
+}
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+        
